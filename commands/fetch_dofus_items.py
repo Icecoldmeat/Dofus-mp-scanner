fe0cfd9dbffd74d5141e6363e8b081f4d1e254c9
icecoldmeat <alsikjezie1@gmail.com>
2025-12-24 09:47:11 +0100

    cache maps

--- commands/fetch_dofus_items.py
+++ commands/fetch_dofus_items.py
@@ -2,7 +2,7 @@ from api.dofusdb import DofusDBClient
 from repository.mongodb import DofusRepository
 
 dbClient = DofusDBClient(DofusRepository(DofusRepository.TABLE_RECIPE))
-dbClient.fetch_all(dbClient.RECIPE_URL, dbClient.RECIPE_TOTAL,1000)
+dbClient.fetch_all(dbClient.RECIPE_URL, dbClient.RECIPE_TOTAL, 1000)
 
 dbClient = DofusDBClient(DofusRepository(DofusRepository.TABLE_ITEM))
-dbClient.fetch_all(dbClient.ITEM_URL, dbClient.ITEM_TOTAL,0)
+dbClient.fetch_all(dbClient.ITEM_URL, dbClient.ITEM_TOTAL, 0)
